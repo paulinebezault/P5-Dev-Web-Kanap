0879bddcc1d53ca7e703f61009d5dc06fe420039
Bezault <bezaultpauline1@gmail.com>
2022-05-27 10:01:29 +0200

    démarrage js

---
+++ front/js/index.js
@@ -0,0 +1,19 @@
+
+fetch("http://localhost:3000/api/products")
+.then(response=>response.json())
+/*fetch a récup la donnée à l'url donnée, 
+quand il a résolu il va dans le then; 
+il attribut le résultat dans la variable response puis la convertit*/
+.then(data=>{
+    const DOMItems=document.getElementById("items")
+    for(let i=0;i<=data.length;i++){
+        let element=data[i] //pour chaque i allant de 0 à la taille du tableau je prends l'élément i du tableau
+    DOMItems.innerHTML+=`<a href="./product.html?id=42">
+    <article>
+      <img src="${element.imageurl}" alt="${element.altTxt}">
+      <h3 class="productName">${element.name}</h3>
+      <p class="productDescription">${element.description}</p>
+    </article>
+  </a>`
+    }
+})
