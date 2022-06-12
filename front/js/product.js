3cd5ef6126e8ab0e5d12617057da3db258b64cc3
Bezault <bezaultpauline1@gmail.com>
2022-06-12 16:56:38 +0200

    page product

--- front/js/product.js
+++ front/js/product.js
@@ -8,4 +8,15 @@ fetch(`http://localhost:3000/api/products/${id}`) // fait la requete avec l'id e
     console.log(data);
     const image=document.getElementsByClassName("item__img")[0];
     image.innerHTML=`<img src="${data.imageUrl}" alt="Photographie d'un canapé"></img>`
+    const title=document.getElementById("title");
+    title.innerHTML=data.name;
+    const price=document.getElementById("price");
+    price.innerHTML=data.price;
+    const description=document.getElementById("description");
+    description.innerHTML=data.description;
+    const colors=document.getElementById("colors");
+    for (let i=0;i<data.colors.length;i++){
+        colors.innerHTML+=`<option value="${data.colors[i]}">${data.colors[i]}</option>`
+    }//pour chaque i commençant de zéro jusqu'à la taille de mon tableau de couleur
+
 })
