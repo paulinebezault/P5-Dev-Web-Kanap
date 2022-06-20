5021d9bb3894d8a7eedf1e2b02556cba6a88bfaf
Bezault <bezaultpauline1@gmail.com>
2022-06-20 12:04:09 +0200

    page produit

--- front/js/product.js
+++ front/js/product.js
@@ -19,4 +19,25 @@ fetch(`http://localhost:3000/api/products/${id}`) // fait la requete avec l'id e
         colors.innerHTML+=`<option value="${data.colors[i]}">${data.colors[i]}</option>`
     }//pour chaque i commençant de zéro jusqu'à la taille de mon tableau de couleur
 
+    const quantity=document.getElementById("quantity");
+    const button=document.getElementById("addToCart");
+    button.onclick=()=>{
+        let previousCart=JSON.parse(localStorage.getItem("cart"));//je vais chercher dans localStorage la clé "cart" je la parse et la mets dans la variable
+        if(previousCart==null){previousCart=[]};//si pas de panier, alors tableau vide
+        
+        let alreadyExist = previousCart.findIndex((el)=>el.id==data._id&&el.selectedColor==colors.value)//si il y a un élément du tableau dont l'id correspond à l'id qu'on cherche et dont la couleur correspond à la couleur qu'on cherche
+        if (alreadyExist >= 0){
+            previousCart[alreadyExist].selectedQuantity +=parseInt(quantity.value) ;//on modifie la quantité
+        }else{
+            let dataToStore={
+            id:data._id,
+            selectedColor:colors.value,
+            selectedQuantity:parseInt(quantity.value)
+        };//crée l'objet à ajouter dans le panier
+          previousCart.push(dataToStore);//on l'ajoute au tableau du panier  
+        }
+        
+        localStorage.setItem("cart",JSON.stringify(previousCart));//on modifie le locastorage avec la nouvelle valeur
+    }//fonction qui se déclenche chaque fois qu'on clique sur le bouton
+
 })
